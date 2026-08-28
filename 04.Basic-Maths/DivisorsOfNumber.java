5a22c350a6e40e4e549c42db7ecb3b551ffd1ada
heynikcse <nikhilraj3103@gmail.com>
2026-08-29 00:05:28 +0530

    problems solve

--- 04.Basic-Maths/DivisorsOfNumber.java
+++ 04.Basic-Maths/DivisorsOfNumber.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class DivisorsOfNumber {
 
+    //1. Method
 //    static void divisors(int n) {
 //
 //        for (int i = 1; i <= n ; i++) {
@@ -11,14 +12,26 @@ public class DivisorsOfNumber {
 //        }
 //    }
 
-    // return in array/list
+    // 2. Method return in array/list
     static int[] divisors(int n) {
 
         ArrayList<Integer> list = new ArrayList<>();
 
-        for(int i = 1 ; i <= n ; i++){
-            if( n % i == 0) {
+//        for(int i = 1 ; i <= n ; i++){
+//            if( n % i == 0) {
+//                list.add(i);
+//            }
+//        }
+
+        // Different looping 1 to root n.
+        for(int i = 1 ; i <= Math.sqrt(n) ; i++){
+
+            if ( n%i == 0) {
                 list.add(i);
+
+                if(i != n/i) {
+                    list.add(n/i);
+                }
             }
         }
 
@@ -27,14 +40,17 @@ public class DivisorsOfNumber {
         for(int i = 0 ; i < list.size() ; i++){
             array[i] = list.get(i) ;
         }
+
+        Arrays.sort(array);
         return array;
     }
 
+
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the numeber : ");
+        System.out.print("Enter the number : ");
         int n = sc.nextInt();
 
-        System.out.print(Arrays.toString(divisors(n)));
+        System.out.println(Arrays.toString(divisors(n)));
     }
 }
