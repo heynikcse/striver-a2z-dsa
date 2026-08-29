52be77d7f956b657c422fb90a9fa661c72b1bf4d
heynikcse <nikhilraj3103@gmail.com>
2026-08-30 03:33:17 +0530

    hash basic by solving counting character problem

--- 06-Basic-Hashing/NumberHash.java
+++ 06-Basic-Hashing/NumberHash.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class CountElement {
+public class NumberHash {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
---
+++ 06-Basic-Hashing/StringHash.java
@@ -0,0 +1,25 @@
+//  -- Declaring hash array and counting character from a string of lowercase --
+
+import java.util.*;
+
+public class StringHash {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string : ");
+        String s = sc.nextLine();
+
+        //Precalculation
+
+        int[] hash = new int[26] ;
+        for (int i = 0 ; i < s.length() ; i++) {
+            hash[s.charAt(i) - 'a']++ ;
+        }
+
+        System.out.print("Enter the character for count : ");
+        char character = sc.next().charAt(0);
+
+        System.out.println(hash[character-'a']);
+
+    }
+}
